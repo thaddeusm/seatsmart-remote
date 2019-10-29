c23e370cbdfbe201d690c1b8e8fc31e2ed1d018c
Thaddeus McCleary <thaddeusmccleary@yahoo.com>
2019-10-29 17:25:58 +0800

    Add illustration

--- src/components/ActivitySelector.vue
+++ src/components/ActivitySelector.vue
@@ -3,7 +3,8 @@
 		<h4>Select an Activity to Launch</h4>
 		<button v-if="!loading" v-for="(activity, index) in activities" @click="launchActivity(index)">
 			<img v-if="activity.activityType == 'survey'" src="@/assets/survey-illustration.svg" alt="survey-illustration">
-			<img v-else src="@/assets/response-pool-illustration.svg" alt="survey-illustration">
+			<img v-else-if="activity.activityType == 'response pool'" src="@/assets/response-pool-illustration.svg" alt="response pool illustration">
+			<img v-else-if="activity.activityType == 'information gap'" src="@/assets/information-gap-illustration.svg" alt="information gap illustration">
 			<h6>{{ shortName(activity.name) }}</h6>
 		</button>
 		<Loader v-if="loading" class="loader" />
